86ee1cf761227aff786d2b755a3e4650d8f49ff2
andluo <1455234749@qq.com>
2019-01-21 23:56:38 +0800

    处理带参数的get请求

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const port = process.argv[2] || 9000
 
 http.createServer((req,res)=>{
     const {method} = req
+    
+
+    //判断Get or Post
     if(method === 'GET'){
         console.log(`这是${method}请求: ${req.url}`)
         let pathObj = url.parse(req.url,true)
@@ -14,21 +17,26 @@ http.createServer((req,res)=>{
         if(Object.keys(query).length === 0){
             //处理不带参数的get请求
             let file_path = path.join(__dirname,pathObj.pathname)
-            fs.readFile(file_path,(err,contnet)=>{
+            fs.readFile(file_path,(err,data)=>{
                 if(err){
                     console.log("There is error.")
                     res.writeHead(404,'Not Find.')
-                    return res.end("404...")
+                    return res.end('<h1>404...</h1>')
                 }
                 
                 res.writeHead(200,'ok')
-                res.write(contnet)
+                res.write(data)
                 return res.end()
 
             })
         }else{
             console.log("带了参数")
             //处理带参数的get请求
+            if(pathObj.pathname === '/test'){
+                res.end(JSON.stringify(query))
+            }else{
+                return res.end('<h1>404...</h1>')
+            }
         }
 
     }else if(method === 'POST'){
