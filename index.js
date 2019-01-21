241d1152991dfe6918c70bb2052087685ba0ed9d
andluo <1455234749@qq.com>
2019-01-22 00:18:57 +0800

    访问/处理

--- index.js
+++ index.js
@@ -8,18 +8,24 @@ const port = process.argv[2] || 9000
 http.createServer((req,res)=>{
     const {method} = req
     
+    req.on('error',(err)=>{
+        console.log(`on error: ${err}`)
+    })
 
     //判断Get or Post
     if(method === 'GET'){
         console.log(`这是${method}请求: ${req.url}`)
         let pathObj = url.parse(req.url,true)
+        
+        req.url = req.url === '/' ? 'index.html':req.url
+
         let query = pathObj.query
         if(Object.keys(query).length === 0){
             //处理不带参数的get请求
             let file_path = path.join(__dirname,pathObj.pathname)
             fs.readFile(file_path,(err,data)=>{
                 if(err){
-                    console.log("There is error.")
+                    console.log(`${req.method} ${req.url} is not find.`)
                     res.writeHead(404,'Not Find.')
                     return res.end('<h1>404...</h1>')
                 }
